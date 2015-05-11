1752e86ba66c5396ee46dcb74a2d74f4d3589b00
Almewty <almewty@gmail.com>
2015-05-11 18:06:33 +0200

    initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float speed;
+	public float sleepVelocity;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+		if (horizontal == 0 && vertical == 0 && rb.angularVelocity.sqrMagnitude < sleepVelocity)
+			rb.angularVelocity = Vector3.zero;
+		else
+			rb.AddForce(movement * speed);
+
+		if (Input.GetButton("Jump"))
+		{
+			rb.AddForce(0f, 100f, 0f);
+		}
+	}
+}
