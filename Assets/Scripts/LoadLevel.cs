af4ee6e122875f3368c9ff5d9b5affd482bbc7bb
Almewty <almewty@gmail.com>
2015-05-14 14:29:49 +0200

    build test level and added switching levels with f1

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public string nextLevel;
+		
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.F1))
+			Application.LoadLevel(nextLevel);
+	}
+}
