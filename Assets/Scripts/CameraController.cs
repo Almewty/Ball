22e8b0f8389c45ecc19fe002392415b00ad50198
Almewty <almewty@gmail.com>
2015-05-14 14:00:17 +0200

    upped anti aliasing and fixed camera controlls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public Transform target;
-	public float relativeX = 0f;
-	public float relativeY = 5f;
-	public float relativeZ = -10f;
+	public float rotationHorizontalModifier = 1;
+	public float rotationVerticalModifier = 3;
+	public Vector3 relativePosition = new Vector3(0f, 0f, -10f);
+	public float horizontalRotation = 15;
+	public float verticalRotation;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +19,15 @@ public class CameraController : MonoBehaviour {
 	void LateUpdate () {
 		if (target == null)
 			return;
-		transform.position = target.position + new Vector3(relativeX, relativeY, relativeZ);
+
+		horizontalRotation += Input.GetAxis("Mouse Y") * rotationHorizontalModifier;
+		horizontalRotation = Mathf.Clamp(horizontalRotation, 0, 45);
+		verticalRotation += Input.GetAxis("Mouse X")* rotationVerticalModifier;
+		verticalRotation %= 360;
+
+		transform.position = target.position + relativePosition;
+		transform.RotateAround(target.position, Vector3.right, horizontalRotation);
+		transform.RotateAround(target.position, Vector3.up, verticalRotation);
 		transform.LookAt(target);
 	}
 }
